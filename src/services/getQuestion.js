073f2ea528e57005888150f2240dcd4cd79c4633
PauloMarxs <paulomarquestelles@gmail.com>
2021-04-05 17:21:46 -0300

    correção no serviço de busca da API para tratamento dos erros

--- src/services/getQuestion.js
+++ src/services/getQuestion.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 export default async function getQuestion() {
   const question = await axios
     .get('https://8zqqb4wng6.execute-api.us-east-1.amazonaws.com/dev/')
-    .then((res) => res.data);
+    .then((res) => res.data)
+    .catch((err) => {
+      throw err;
+    });
   return question;
 }
