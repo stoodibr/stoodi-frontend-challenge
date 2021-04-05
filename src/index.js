264275744826371930ea51058da070f789d035ee
PauloMarxs <paulomarquestelles@gmail.com>
2021-04-05 17:29:01 -0300

    alteração para chamar a pagina no index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyle from './globalStyles';
+import MainPage from './pages/Main';
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
+    <MainPage />
   </React.StrictMode>,
   document.getElementById('root'),
 );
